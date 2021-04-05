f1d1293f517d22b12a20a3688810fe3e8a791215
Mauro Torrez <maurotorrez@gmail.com>
2021-04-04 23:58:34 -0300

    complete Dockerfile, entrypoint.sh, action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,47 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+set -e
+
+# fail if INPUT_PLAYBOOK is not set
+if [[ -z "${INPUT_PLAYBOOK}" ]]
+then
+  echo "Required variable \`playbook\` is missing"
+  exit 1
+fi
+
+if [[ -n "${INPUT_WORKING_DIRECTORY}" ]]
+then
+  cd "${INPUT_WORKING_DIRECTORY}"
+fi
+
+if [[ -n "${INPUT_AWS_ACCESS_KEY_ID}" ]]
+then
+  export AWS_ACCESS_KEY_ID="${INPUT_AWS_ACCESS_KEY_ID}"
+fi
+
+if [[ -n "${INPUT_AWS_SECRET_ACCESS_KEY}" ]]
+then
+  export AWS_SECRET_ACCESS_KEY="${INPUT_AWS_SECRET_ACCESS_KEY}"
+fi
+
+if [[ -n "${INPUT_AWS_REGION}" ]]
+then
+  export AWS_REGION="${INPUT_AWS_REGION}"
+fi
+
+if [[ -n "${INPUT_AWS_SESSION_TOKEN}" ]]
+then
+  export AWS_SESSION_TOKEN="${INPUT_AWS_SESSION_TOKEN}"
+fi
+
+if [[ -z "${INPUT_ARGUMENTS}" ]]
+ then
+  COMMAND="ansible-playbook ${INPUT_PLAYBOOK}"
+else
+  COMMAND="ansible-playbook ${INPUT_ARGUMENTS} ${INPUT_PLAYBOOK}"
+fi
+
+echo "::debug:: Executing command: ${COMMAND}"
+echo ::set-output name=command::${COMMAND}
+
+${COMMAND}
